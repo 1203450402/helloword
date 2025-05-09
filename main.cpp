d7b309c431181315f57c1def465c6d8ac203f84e
ys <1203450402@qq.com>
2025-05-09 20:44:27 +0800

    创建main.cpp文件，写了初始的代码

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void sort(int arr[],int size){
+	
+}
+
+
+
+int main(){
+	int arr[]={12,3,4,31,24};
+	int size=sizeof(arr)/sizeof(arr[0]);
+	sort(arr,size);
+	return 0;
+}
+
