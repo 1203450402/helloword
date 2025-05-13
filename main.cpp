2fe3126784871b786ac9b09dd14aaf8b11366c58
ys <1203450402@qq.com>
2025-05-13 16:39:10 +0800

    改为快速排序

--- main.cpp
+++ main.cpp
@@ -3,16 +3,7 @@ using namespace std;
 
 // 阿狸添加注释
 void sort(int arr[],int size){
-	int tmp=0;
-	for(int i=0;i<size-1;++i){
-		for(int j=0;j<size-1;j++){
-			if(arr[j]>arr[j+1]){
-				tmp=arr[j];
-				arr[j]=arr[j+1];
-				arr[j+1]=tmp;
-			}
-		}
-	}
+	// 快速排序
 }
 
 
